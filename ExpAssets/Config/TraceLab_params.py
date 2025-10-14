61f2b454a76848573b6b9183323456c2502b702e
Austin Hurst <mynameisaustinhurst@gmail.com>
2025-10-14 14:18:43 -0300

    Revert development params

--- ExpAssets/Config/TraceLab_params.py
+++ ExpAssets/Config/TraceLab_params.py
@@ -9,7 +9,7 @@ collect_demographics = True
 manual_demographics_collection = True
 manual_trial_generation = True
 run_practice_blocks = False
-force_show_cursor = True # If False, cursor hidden unless in devmode & no touchscreen
+force_show_cursor = False # If False, cursor hidden unless in devmode & no touchscreen
 
 mirror_mode = False
 enable_learned_figures_querying = True
@@ -201,7 +201,7 @@ session_structures = {
     ],
     'b': [
         # Session 1
-        [#'PP-Practice',
+        ['PP-Practice',
          'MI-F3', 'MI-F3', 'MI-F3', 'MI-F3', 'MI-F3'],
         # Session 2
         ['MI-F3', 'MI-F3', 'MI-F3', 'MI-F3', 'MI-F3'],
